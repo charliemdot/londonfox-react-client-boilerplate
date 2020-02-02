c7de1b4544649e1195563846a9030f094c6a606d
cmarkovich <cmarkovich@gmail.com>
2020-02-01 20:55:37 -0800

    Nearly running, added new support for Webpack4 with Babel

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
+
+const moduleObj = {
+  rules: [
+    // JavaScript/JSX Files
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader'],
+    },
+    // CSS Files
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader'],
+    }
+  ]
+}
+
+const client = {
+  entry: {
+    'client': './src/client/index.js'
+  },
+  target: 'web',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist/public')
+  },
+  module: moduleObj,
+  // Plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'LondonFox React Boilerplate'
+    })
+  ],
+  // Reload On File Change
+  watchOptions: {
+    ignored: /node_modules/
+  },
+  // Development Tools (Map Errors To Source File)
+  devtool: 'source-map',
+}
+
+const server = {
+  entry: {
+    'server': './src/server/index.js'
+  },
+  target: 'node',
+  // Do not minify node js files
+  mode: 'development',
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  externals: [nodeExternals()]
+}
+
+module.exports = [client, server]
