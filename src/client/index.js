1b17bc7985b2c234757454b5443458a3601a223f
cmarkovich <cmarkovich@gmail.com>
2020-02-18 22:32:17 -0800

    Adding file name check to ESLint

--- src/client/index.js
+++ src/client/index.js
@@ -1,18 +1,18 @@
-import React from 'react'
-import ReactDOM from "react-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import { setContext } from 'apollo-link-context';
-import { Reset } from 'styled-reset'
-import { createGlobalStyle } from 'styled-components'
+import { Reset } from 'styled-reset';
+import { createGlobalStyle } from 'styled-components';
 
 import { GlobalLayout } from './layouts/global';
 import { AUTH_TOKEN } from './utils/constants';
 
 const httpLink = createHttpLink({
-  uri: 'https://namify-api-service-prod.herokuapp.com/api'
+  uri: 'https://namify-api-service-prod.herokuapp.com/api',
 });
 
 const authLink = setContext((_, { headers }) => {
--- src/client/utils/constants.js
+++ src/client/utils/constants.js
@@ -1,1 +1,3 @@
 export const AUTH_TOKEN = 'auth-token';
+
+// TODO: Move this into contstants directory instead of utils
